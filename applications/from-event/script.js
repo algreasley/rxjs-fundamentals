a84713fbd0d0a247c0403132978fc9e207f3c92c
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-09 22:59:26 +0000

    Ex 2: fromEvent notification app

--- applications/from-event/script.js
+++ applications/from-event/script.js
@@ -3,14 +3,14 @@ import { fromEvent } from 'rxjs';
 const button = document.getElementById('create-notification');
 const notificationMessages = document.getElementById('notification-messages');
 
-const createNotificationElement = () => {
+const createNotificationElement = (msg) => {
   const element = document.createElement('article');
-  element.innerText = 'Something happened.';
+  element.innerText = `Something happened: ${msg}`;
   return element;
 };
 
-const addMessageToDOM = () => {
-  const notification = createNotificationElement();
+const addMessageToDOM = (msg) => {
+  const notification = createNotificationElement(msg);
   notificationMessages.appendChild(notification);
 };
 
@@ -22,3 +22,8 @@ const addMessageToDOM = () => {
  * - Use `addMessageToDOM` to add a useless message to the DOM whenever the
  *   stream emits a value.
  */
+
+let index = 0;
+fromEvent(button, 'click').subscribe((event) =>
+  addMessageToDOM(`event ${index++}`),
+);
