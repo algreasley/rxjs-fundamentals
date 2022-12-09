d90bdb2fa6322442586febc3b927a957257de0fb
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-09 23:33:44 +0000

    Ex 3: basic counter app
    Got it pretty close to Steve's ... this is a bit more risk-averse re clicking start/pause multiple times

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,20 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let subscription;
+const timer$ = interval(1000);
+start$.subscribe(() => {
+  if (!subscription) {
+    subscription = timer$.subscribe((intervalCount) => {
+      setCount(intervalCount);
+    });
+  }
+});
+
+pause$.subscribe(() => {
+  if (subscription) {
+    subscription.unsubscribe();
+    subscription = undefined;
+  }
+});
