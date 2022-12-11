ca7d936c1a660966ac5df372a4258478a06238c1
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-11 18:03:41 +0000

    Ex 0 Basic Observables tests .. solutions in creating observables lesson

--- exercises/basic-observables.test.js
+++ exercises/basic-observables.test.js
@@ -2,16 +2,21 @@ import { from, of } from 'rxjs';
 
 describe('Basic Observables', () => {
   describe(of, () => {
-    it.skip('should create an observable from its arguments', () => {
+    it('should create an observable from its arguments', () => {
       const result = [];
 
+      const items$ = of(1, 2, 3, 4);
+      items$.subscribe((val) => result.push(val));
       expect(result).toEqual([1, 2, 3, 4]);
     });
   });
 
   describe(from, () => {
-    it.skip('should create an observable', () => {
+    it('should create an observable', () => {
       const result = [];
+
+      const items$ = from([1, 2, 3, 4]);
+      items$.subscribe((val) => result.push(val));
 
       expect(result).toEqual([1, 2, 3, 4]);
     });
