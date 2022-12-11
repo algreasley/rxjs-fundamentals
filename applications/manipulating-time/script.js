d08a52c7d2288859a55996a45805747561a418d7
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-11 22:32:12 +0000

    Int: manipulating time - debounce/throttle

--- applications/manipulating-time/script.js
+++ applications/manipulating-time/script.js
@@ -19,6 +19,16 @@ import {
   setStatus,
 } from './utilities';
 
-const buttonClicks$ = fromEvent(button, 'click');
+// delay forever until "Panic Button" clicked
+// const panicClicks$ = fromEvent(panicButton, 'click');
+// const buttonClicks$ = fromEvent(button, 'click').pipe(
+//   debounce(() => panicClicks$),
+// );
+
+// don't emit for 2s after an initial click .. repeats
+const buttonClicks$ = fromEvent(button, 'click').pipe(throttleTime(2000));
+
+// wait for 2s clear time
+// const buttonClicks$ = fromEvent(button, 'click').pipe(debounceTime(2000));
 
 buttonClicks$.subscribe(addMessageToDOM);
