4403dbf1ecd2bef78d2489695b5fd3069ec9c672
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-11 22:13:13 +0000

    Ex 5: basic counter app++ .. added skipUnti, takeUntil, scan

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,22 +1,15 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import { scan, skipUntil, takeUntil } from 'rxjs/operators';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
-let subscription;
-const timer$ = interval(1000);
-start$.subscribe(() => {
-  if (!subscription) {
-    subscription = timer$.subscribe((intervalCount) => {
-      setCount(intervalCount);
-    });
-  }
-});
-
-pause$.subscribe(() => {
-  if (subscription) {
-    subscription.unsubscribe();
-    subscription = undefined;
-  }
-});
+// obvs issues, start/pause only once as using *Until
+const timer$ = interval(1000)
+  .pipe(
+    skipUntil(start$),
+    takeUntil(pause$),
+    scan((total) => total + 1, 0),
+  )
+  .subscribe(setCount);
