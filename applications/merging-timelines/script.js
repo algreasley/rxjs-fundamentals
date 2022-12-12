597076ecc900701a1318bab199b649bc2bdc35fa
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-12 10:02:01 +0000

    Int: merging timelines - merge, concat, forkjoin

--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -6,4 +6,23 @@ import {
   pauseButton,
   setStatus,
   bootstrap,
+  clearButton,
 } from './utilities';
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+const clear$ = fromEvent(clearButton, 'click');
+
+merge(start$, pause$)
+  .pipe(startWith(false))
+  .subscribe((val) => setStatus(val));
+
+const first$ = interval(1500).pipe(map(labelWith('First')), take(3));
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(3));
+
+// const combined$ = merge(first$, second$);
+// const combined$ = concat(first$, second$);
+// const combined$ = race(first$, second$);
+const combined$ = forkJoin(first$, second$);
+
+bootstrap({ first$, second$, combined$ });
