b89fd62b8d5f74ec4171d843b17edd8f15e7b8c4
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-13 09:50:23 +0000

    Int: dog facts, fetch / stop .. basic .. code-along

--- applications/dog-facts/script.js
+++ applications/dog-facts/script.js
@@ -24,30 +24,31 @@ import {
 
 const endpoint = 'http://localhost:3333/api/facts?delay=1000&flakiness=2';
 
-const fetch$ = fromEvent(fetchButton, 'click');
+const fetch$ = fromEvent(fetchButton, 'click').pipe(map(() => true));
+const stop$ = fromEvent(stopButton, 'click').pipe(map(() => false));
 
-fetch$
+const fetchData$ = fromFetch(endpoint).pipe(
+  mergeMap((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('something bad happened :(');
+    }
+  }),
+  retry(4),
+  // catchError here otherwise completes the outer observable
+  catchError((error) => {
+    return of({ error: error.message });
+  }),
+);
+
+merge(fetch$, stop$)
   .pipe(
     tap(() => {
       clearFacts();
       clearError();
     }),
-    mergeMap(() =>
-      fromFetch(endpoint).pipe(
-        mergeMap((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('something bad happened :(');
-          }
-        }),
-        retry(4),
-        // catchError here otherwise completes the outer observable
-        catchError((error) => {
-          return of({ error: error.message });
-        }),
-      ),
-    ),
+    switchMap((shouldFetch) => (shouldFetch ? fetchData$ : NEVER)),
   )
   .subscribe(({ facts, error }) => {
     if (facts) {
