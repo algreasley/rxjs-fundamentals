7dd435ba3c5004a917ea13afa3a63cda081709d4
Alan Greasley <alan.greasley@weareadaptive.com>
2022-12-13 09:11:33 +0000

    Int: dog facts, delay, flakiness .. code-along

--- applications/dog-facts/script.js
+++ applications/dog-facts/script.js
@@ -22,14 +22,27 @@ import {
   setError,
 } from './utilities';
 
-const endpoint = 'http://localhost:3333/api/facts';
+const endpoint = 'http://localhost:3333/api/facts?delay=1000&flakiness=2';
 
 const fetch$ = fromEvent(fetchButton, 'click');
 
 fetch$
   .pipe(
     mergeMap(() => fromFetch(endpoint)),
-    mergeMap((response) => response.json()),
-    tap((val) => console.log(val)),
+    mergeMap((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return of({ error: 'something bad happened :(' });
+      }
+    }),
   )
-  .subscribe(addFacts);
+  .subscribe(({ facts, error }) => {
+    clearFacts();
+    clearError();
+    if (facts) {
+      addFacts({ facts });
+    } else {
+      setError(error);
+    }
+  });
